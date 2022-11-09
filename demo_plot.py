986c8ce4d0ec9a9da18ade3d2c0480cdda611c93
393061143@qq.com <393061143@qq.com>
2022-11-09 20:34:46 +0800

    更新绘图

--- demo_plot.py
+++ demo_plot.py
@@ -8,14 +8,27 @@ import imageio
 asset_tgt = ['000001']
 desti_path = './'
 
-headers = ['date', 'tid', 'open', 'high', 'low', 'close', 'volume', 'mid', 'range', 'fac0', 'avg', 's3', 's7', 's14',
-           's21', 's35', 's49',
-           's75', 's100', 's150', 's200', 's300', 's400', 's500', 's600', 's800', 's1000', 's1500', 's2000', 's3000',
-           'ma_close', 'ub', 'lb']
-
-lm_hd = ['s3000', 's2000', 's1500', 's1000', 's800', 's600', 's500', 's400', 's300', 's200', 's150', 's100', 's75',
-         's49', 's14', 'ub', 'lb']
-sm_hd = ['s1000', 's800', 's600', 's500', 's400', 's300', 's200', 's150', 's100', 's75', 's49', 's14', 'ub', 'lb']
+shift_num = 50
+config = {}
+config['close_px'] = dict(linewidth=0.7, label='close', color='#767676',secondary_y=True)
+config['close_pxma'] = dict(linewidth=2, label='ma_close', color='#767676',secondary_y=True)
+config['s49'] = dict(linewidth=2.3, label='s49', color='#FFFFFF', secondary_y=False)
+config['s75'] = dict(linewidth=2.3, label='s75', color='#008080', secondary_y=False)
+config['s100'] = dict(linewidth=2.3, label='s100', color='#FFFF00',secondary_y=False)
+config['s150'] = dict(linewidth=4.5, label='s150', color='#FF0000',secondary_y=False)  # original_lwt==4
+config['s200'] = dict(linewidth=4.5, label='s200', color='#000000',secondary_y=False)  # original_lwt==4
+config['s300'] = dict(linewidth=2.1, label='s300', color='#800080',secondary_y=False)
+config['s400'] = dict(linewidth=2.1, label='s400', color='#800080',secondary_y=False)
+config['s500'] = dict(linewidth=2.1, label='s500', color='#800080',secondary_y=False)
+config['s600'] = dict(linewidth=2.1, label='s600', color='#800000',secondary_y=False)
+config['s800'] = dict(linewidth=2.1, label='s800', color='#993300',econdary_y=False)
+config['s1000'] = dict(linewidth=4.5, label='s1000', color='#9090FF',secondary_y=False)  # original_lwt==4.2
+config['s1500'] = dict(linewidth=2.1, label='s1500', color='#FF99CC',secondary_y=False)  # original_lwt==1.8
+config['s2000'] = dict(linewidth=2.1, label='s2000', color='#FFCC99',secondary_y=False)  # original_lwt==1.8
+config['s3000'] = dict(linewidth=4.5, label='s3000', color='#FF6600',secondary_y=False)  # original_lwt==4.2
+config['ub'] = dict(linewidth=1.8, label='ub', color='#0000FF', secondary_y=False)
+config['lb'] = dict(linewidth=1.8, label='lb', color='#0000FF', secondary_y=False)
+config['x'] = dict(linewidth=0.5, label='0_hori', color='#000000', secondary_y=False)
 
 
 def fig2img(fig):
@@ -165,29 +178,48 @@ def pic_out(df, if_mirror):
     return img
 
 
-def plot(lst, l):
+
+
+
+def plot(d, l):
     ax = []  
     ay = [] 
     plt.ion()  
-    plt.ylim(1300)
-    for i in range(0, len(lst), 100):  
-        if i > 3000:
-            plt.xlim(i-3000)
-        ax = [i for i in range(i, i+100)]  
-        ay = lst[i:i+100]  
-        print(len(ax), len(ay))
-        plt.plot(ax, ay, linewidth=0.7, label='close', color='#767676')  
+    plt.ylim(500)
+    for i in range(0, len(d['s100']), shift_num):  
+        for key in d:
+            lst = d[key]
+            ax = [i for i in range(i, i+shift_num)]  
+            ay = lst[i:i+shift_num]  
+            plt.plot(ax, ay, linewidth=config[key]['linewidth'], label=config[key]['label'], color=config[key]['color']) 
+            aax = plt.gca()
+            aax.invert_xaxis()
         plt.pause(0.001)  
-        fig = plt.gcf()
-        l.append(fig2img(fig))
+        # fig = plt.gcf()
+        # l.append(fig2img(fig))
         plt.ioff()  
     return l
 
+
+headers = ['date', 'tid', 'open', 'high', 'low', 'close', 'volume', 'mid', 'range', 'fac0', 'avg', 's3', 's7', 's14',
+           's21', 's35', 's49',
+           's75', 's100', 's150', 's200', 's300', 's400', 's500', 's600', 's800', 's1000', 's1500', 's2000', 's3000',
+           'ma_close', 'ub', 'lb']
+
+lm_hd = ['s3000', 's2000', 's1500', 's1000', 's800', 's600', 's500', 's400', 's300', 's200', 's150', 's100', 's75',
+         's49', 's14', 'ub', 'lb']
+sm_hd = ['s1000', 's800', 's600', 's500', 's400', 's300', 's200', 's150', 's100', 's75', 's49', 's14', 'ub', 'lb']
+
+plot_list = ['s1000', 's800', 's600', 's500', 's400', 's300', 's200', 's150', 's100', 's75', 's49', 'ub', 'lb', 'x']
 df = pd.read_csv("000001_30_t4_out.csv")
-s300 = df['s300'].tolist()
-print(len(s300))
-l = plot(s300, [])
-print(l)
+df['x'] = 0
+df.sort_values(by='date',axis=0, ascending=True, inplace=True)
+
+d = {i:df[i].tolist() for i in plot_list}
+
+
+l = plot(d, [])
+# print(l)
 
 # img = pic_out(df, False)
 # Image.Image.save(img, "out.png")
