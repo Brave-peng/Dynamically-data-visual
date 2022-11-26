6b1c6ab67bd96768eb9b518500322dca59ad21fa
Brave-peng <primer_9@qq.com>
2022-11-26 22:23:00 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
-# Plot
+# 使用演示
+
+#### 使用演示
+
+![](display.gif)
+
+
 
 #### 介绍
-a simple animal Plot code
+
+一个简单的实时动态绘图软件，利用pyqt+plt完成。
+这个项目是自己给别人做的需求的简化版本，主要用于动态性的展示数据的变化趋势，支持跳转定位、倒播、数据镜像等功能。
 
 #### 软件架构
 软件架构说明
 
 
-#### 安装教程
+#### 安装须知
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+你可能需要配置的环境：pyqt5、matplotlib、pandas、numpy
+你可以使用以下命令尝试安装：
+pip3 install pyqt5 matplotlib pandas numpy
 
 #### 使用说明
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  运行main.py即可
 
 #### 参与贡献
 
@@ -26,12 +32,3 @@ a simple animal Plot code
 3.  提交代码
 4.  新建 Pull Request
 
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
--- main.py
+++ main.py
@@ -19,27 +19,12 @@ SPEED = 10
 WINDOW_LENGTH = 3000
 TICK_SPACING = 100
 Y_SIZE = 16
+SECOND_Y =  ['ma_close']
+
 config = {}
-config['close'] = dict(linewidth=0.7, label='close', color='#767676',secondary_y=True)
 config['ma_close'] = dict(linewidth=2, label='ma_close', color='#767676',secondary_y=True)
-config['s49'] = dict(linewidth=2.3, label='s49', color='#FFFFFF', secondary_y=False)
-config['s75'] = dict(linewidth=2.3, label='s75', color='#008080', secondary_y=False)
-config['s100'] = dict(linewidth=2.3, label='s100', color='#FFFF00',secondary_y=False)
-config['s150'] = dict(linewidth=4.5, label='s150', color='#FF0000',secondary_y=False)  # original_lwt==4
-config['s200'] = dict(linewidth=4.5, label='s200', color='#000000',secondary_y=False)  # original_lwt==4
-config['s300'] = dict(linewidth=2.1, label='s300', color='#800080',secondary_y=False)
-config['s400'] = dict(linewidth=2.1, label='s400', color='#800080',secondary_y=False)
-config['s500'] = dict(linewidth=2.1, label='s500', color='#800080',secondary_y=False)
-config['s600'] = dict(linewidth=2.1, label='s600', color='#800000',secondary_y=False)
-config['s800'] = dict(linewidth=2.1, label='s800', color='#993300',econdary_y=False)
-config['s1000'] = dict(linewidth=4.5, label='s1000', color='#9090FF',secondary_y=False)  # original_lwt==4.2
-config['s1500'] = dict(linewidth=2.1, label='s1500', color='#FF99CC',secondary_y=False)  # original_lwt==1.8
-config['s2000'] = dict(linewidth=2.1, label='s2000', color='#FFCC99',secondary_y=False)  # original_lwt==1.8
-config['s3000'] = dict(linewidth=4.5, label='s3000', color='#FF6600',secondary_y=False)  # original_lwt==4.2
-config['ub'] = dict(linewidth=1.8, label='ub', color='#0000FF', secondary_y=False)
-config['lb'] = dict(linewidth=1.8, label='lb', color='#0000FF', secondary_y=False)
-config['x'] = dict(linewidth=0.5, label='0_hori', color='#000000', secondary_y=False)
-plot_list = ['close', 'ma_close', 's1000', 's800', 's600', 's500', 's400', 's300', 's200', 's150', 's100', 's75', 's49', 'ub', 'lb', 'x']
+config['s3'] = dict(linewidth=2.3, label='s49', color='#FFFFFF', secondary_y=False)
+plot_list = ['ma_close', 's3']
 
 class Main(QWidget):
     def __init__(self) -> None:
@@ -93,7 +78,7 @@ class Main(QWidget):
             for key in self.d:
                 ay = self.d[key][self.index: self.index + WINDOW_LENGTH] 
                 
-                if key not in ['close', 'ma_close']:
+                if key not in SECOND_Y:
                     top1 = max(top1, max(ay))
                     bottom1 = min(bottom1, min(ay))
                 else:
@@ -130,14 +115,14 @@ class Main(QWidget):
             self.df = pd.read_csv(self.file, index_col='date')
             self.df['x'] = 0
             self.df.sort_values(by='date', axis=0, ascending=True, inplace=True)
-
+            print(self.df.keys())
             self.d = {i : self.df[i].tolist() for i in plot_list}
             ax = []  
             ay = [] 
             for key in self.d:
                 ax = [i for i in range(len(self.d[key]))]  
                 ay = self.d[key]  
-                if key not in ['close', 'ma_close']:
+                if key not in SECOND_Y:
                     self.ax.plot(ax, ay, linewidth=config[key]['linewidth'], label=config[key]['label'], color=config[key]['color']) 
                     aax = plt.gca()
                     aax.invert_xaxis()
@@ -152,8 +137,8 @@ class Main(QWidget):
         self.logger.info("初始化完毕")
 
     def check_border(self):
-        if self.index + WINDOW_LENGTH >= len(self.d['s800']):
-            self.index = len(self.d['s800']) - WINDOW_LENGTH
+        if self.index + WINDOW_LENGTH >= len(self.d['s3']):
+            self.index = len(self.d['s3']) - WINDOW_LENGTH
         if self.index <= 0:
             self.index = 0
 
@@ -183,7 +168,7 @@ class Main(QWidget):
         for key in self.d:
             ax = [i for i in range(len(self.d[key]))]  
             ay = self.d[key]  
-            if key not in ['close', 'ma_close']:
+            if key not in SECOND_Y:
                 self.ax.plot(ax, ay, linewidth=config[key]['linewidth'], label=config[key]['label'], color=config[key]['color']) 
                 aax = plt.gca()
                 aax.invert_xaxis()
@@ -197,7 +182,7 @@ class Main(QWidget):
         for key in self.d:
             ay = self.d[key][self.index: self.index + WINDOW_LENGTH] 
             
-            if key not in ['close', 'ma_close']:
+            if key not in SECOND_Y:
                 top1 = max(top1, max(ay))
                 bottom1 = min(bottom1, min(ay))
             else:
@@ -237,7 +222,7 @@ class Main(QWidget):
             for key in self.d:
                 ay = self.d[key][self.index: self.index + WINDOW_LENGTH] 
                 
-                if key not in ['close', 'ma_close']:
+                if key not in SECOND_Y:
                     top1 = max(top1, max(ay))
                     bottom1 = min(bottom1, min(ay))
                 else:
